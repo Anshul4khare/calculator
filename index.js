557195895096e3fd2d0a36e683701aec605d5f7e
anshul4khare <anshul.khare040284@gmail.com>
2022-03-04 19:14:11 +0530

    project submitted

---
+++ index.js
@@ -0,0 +1,18 @@
+//getnumer
+
+function number(num){
+  var result = document.getElementById('screen');
+  result.value += num;}
+
+
+//get result
+
+function result (){
+  var result = document.getElementById('screen');
+  result.value = eval(result.value)}
+
+//clear result
+
+function clearresult (){
+  var result = document.getElementById('screen');
+  result.value = ""}
